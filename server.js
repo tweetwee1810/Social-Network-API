4e38109137ff73463e4dc59b90e224083b9cc7b5
Mona Lisa <tweetwee1996@gmail.com>
2023-03-26 19:50:27 -0700

    folder created

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('./config/connection');
+// have not done with the routes
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`running on http://localhost:${PORT}!`);
+    });
+  });
